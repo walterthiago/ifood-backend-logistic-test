ffd8e6dc4212f7cd8594d78d6ef056746851a985
Walter Thiago Andrade Reis <p000wreis@prservicos.com.br>
2019-01-24 16:21:43 -0200

    Initial commit

---
+++ src/main/java/br/com/ifood/ibtlvrptest/ibtlvrptest/IbtlvrptestApplication.java
@@ -0,0 +1,14 @@
+package br.com.ifood.ibtlvrptest.ibtlvrptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbtlvrptestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbtlvrptestApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/br/com/ifood/ibtlvrptest/ibtlvrptest/IbtlvrptestApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.ifood.ibtlvrptest.ibtlvrptest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IbtlvrptestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
